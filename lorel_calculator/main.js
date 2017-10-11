67494f2924d70806d634b91c580c272469b784f4
Priyank Dalal <priyankkumar88@gmail.com>
2017-10-11 22:17:05 +0530

    initial js commit

--- lorel_calculator/main.js
+++ lorel_calculator/main.js
@@ -7,3 +7,27 @@ function drop_handler(ev) {
  ev.srcElement.style.top=ev.clientY+ "px";
  ev.srcElement.style.left=(ev.clientX-(parseInt(ev.srcElement.offsetWidth/2)))+ "px";
 }
+(function(w){
+  function LorelCalculator(){
+    var currentResult=0;
+    var prevResult=0;
+  }
+  LorelCalculator.prototype.animate=function(){};
+  LorelCalculator.prototype.slideUp=function(el,eta){
+    var h=el.offsetHeight-5;
+    var pulse=parseInt(h/40);
+    var t=0;
+    var timer=setInterval(function(){
+      if(t>eta)
+        timer.clearInterval();
+      var mt=el.style.marginTop;
+      mt=parseInt(mt.substr(0,mt.length-2));
+      el.style.marginTop=mt+"px";
+      t+=40;
+    },40);
+  };
+  LorelCalculator.prototype.slideDown=function(el,eta){
+
+  };
+  w['lorel_calculator']= new LorelCalculator();
+})(window);
