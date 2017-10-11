81be50e69313ed70acfc927845d2f8e8de949544
priyank <priyank@edooni.com>
2017-10-11 17:17:53 +0530

    added drag to lorel-widget

--- lorel_calculator/main.js
+++ lorel_calculator/main.js
@@ -2,3 +2,8 @@
 @@@@
 js for lorel calculator
 */
+function drop_handler(ev) {
+ ev.preventDefault();
+ ev.srcElement.style.top=ev.clientY+ "px";
+ ev.srcElement.style.left=(ev.clientX-(parseInt(ev.srcElement.offsetWidth/2)))+ "px";
+}
