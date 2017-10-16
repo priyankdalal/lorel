e69787d256d7e79cd812a5c410f5bc97e7b50326
priyank <priyank@edooni.com>
2017-10-16 14:05:33 +0530

    added drawer animation to calculator

--- lorel_calculator/main.js
+++ lorel_calculator/main.js
@@ -7,51 +7,37 @@ function drop_handler(ev) {
  ev.srcElement.style.top=ev.clientY+ "px";
  ev.srcElement.style.left=(ev.clientX-(parseInt(ev.srcElement.offsetWidth/2)))+ "px";
 }
-function keypad_toggle(){
-  var drawer=document.getElementsByClassName("lorel-drawer")[0];
-  var state=drawer.getAttribute("data-toggle");
-  var ribbon=document.getElementsByClassName("lorel-row")[0];
-  var keypad=document.getElementsByClassName("lorel-row")[1];
-  if(state=="1"){
-    ribbon.classList.add("mem-ribbon-hide");
-    ribbon.classList.remove("mem-ribbon-show");
-    keypad.classList.add("basic-keypad-hide");
-    keypad.classList.remove("basic-keypad-show");
-    drawer.setAttribute("data-toggle","0");
-    drawer.children[0].classList.add("rotate-plus");
-    drawer.children[0].classList.remove("rotate-minus");
-  }else{
-    ribbon.classList.add("mem-ribbon-show");
-    ribbon.classList.remove("mem-ribbon-hide");
-    keypad.classList.add("basic-keypad-show");
-    keypad.classList.remove("basic-keypad-hide");
-    drawer.setAttribute("data-toggle","1");
-    drawer.style.transform="rotateZ(0deg)";
-    drawer.children[0].classList.add("rotate-minus");
-    drawer.children[0].classList.remove("rotate-plus");
-  }
-}
 (function(w){
   function LorelCalculator(){
     var currentResult=0;
     var prevResult=0;
   }
-  LorelCalculator.prototype.animate=function(){};
-  LorelCalculator.prototype.slideUp=function(el,eta){
-    var h=el.offsetHeight-5;
-    var pulse=parseInt(h/40);
-    var t=0;
-    var timer=setInterval(function(){
-      if(t>eta)
-        timer.clearInterval();
-      var mt=el.style.marginTop;
-      mt=parseInt(mt.substr(0,mt.length-2));
-      el.style.marginTop=mt+"px";
-      t+=40;
-    },40);
+  LorelCalculator.drawerState="opened";
+  LorelCalculator.prototype.init=function(){
+
   };
-  LorelCalculator.prototype.slideDown=function(el,eta){
-    //-TODO
+  LorelCalculator.prototype.toggle_keypad=function(){
+    var drawer=document.getElementById("lorel-drawer");
+    var mRibbon=document.getElementById("lorel-mem-ribbon");
+    var bKeypad=document.getElementById("lorel-basic-keypad");
+    if(LorelCalculator.drawerState=="closed"){
+      mRibbon.classList.add("mem-ribbon-show");
+      mRibbon.classList.remove("mem-ribbon-hide");
+      bKeypad.classList.add("basic-keypad-show");
+      bKeypad.classList.remove("basic-keypad-hide");
+      LorelCalculator.drawerState="opened";
+      drawer.style.transform="rotateZ(0deg)";
+      drawer.children[0].classList.add("rotate-minus");
+      drawer.children[0].classList.remove("rotate-plus");
+    }else{
+      mRibbon.classList.add("mem-ribbon-hide");
+      mRibbon.classList.remove("mem-ribbon-show");
+      bKeypad.classList.add("basic-keypad-hide");
+      bKeypad.classList.remove("basic-keypad-show");
+      LorelCalculator.drawerState="closed";
+      drawer.children[0].classList.add("rotate-plus");
+      drawer.children[0].classList.remove("rotate-minus");
+    }
   };
   w['lorel_calculator']= new LorelCalculator();
 })(window);
